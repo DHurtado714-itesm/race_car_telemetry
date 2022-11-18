b0444679465e0ee788f6a05c98fb9d779892fc0c
Daniel Hurtado <daniel_hurtado@Ninja-Hurtado.localdomain>
2022-11-17 23:11:51 -0600

    Database prototype

--- telemtry.sql
+++ telemtry.sql
@@ -5,10 +5,16 @@ CREATE DATABASE telemetry;
 -- Path: telemtry.sql
 
 CREATE TABLE temperature (
-  id SERIAL PRIMARY KEY,
+  id INT PRIMARY KEY, --AUTO_INCREMENT,
   temperature_c NUMERIC(5,1),
-  timestamp TIMESTAMP
+  x NUMERIC(5,1),
+  y NUMERIC(5,1),
+  speed NUMERIC(5,1),
+  date TIMESTAMP
 );
+
+-- conectar la temperatura con la aceleracion y la posicion,
+-- para poder hacer un grafico de la temperatura en funcion de la posicion y la aceleracion
 
 CREATE TABLE aceleration (
   id SERIAL PRIMARY KEY, -- Serial is 1 to n
@@ -26,21 +32,52 @@ CREATE TABLE gps (
 );
 
 CREATE TABLE race(
-  id SERIAL PRIMARY KEY, -- Formato: Lugar-dia-mes-anio (CDMX201022)
+  id INT PRIMARY KEY,
   name VARCHAR(40) NOT NULL,
   location VARCHAR(40) NOT NULL,
+  track_img_path VARCHAR,
   date DATE NOT NULL
 );
 
 
 -- FK
 ALTER Table temperature
-ADD CONSTRAINT fk_raceId FOREIGN KEY(id) REFERENCES race(id);
+ADD CONSTRAINT fk_raceId1 FOREIGN KEY(id) REFERENCES race(id);
 
 ALTER TABLE aceleration
-ADD CONSTRAINT fk_raceId FOREIGN KEY(id) REFERENCES race(id);
+ADD CONSTRAINT fk_raceId2 FOREIGN KEY(id) REFERENCES race(id);
 
-ALTER TABLE gps
+ALTER Table gps
+ADD CONSTRAINT fk_raceId3 FOREIGN KEY(id) REFERENCES race(id);
+
+-- Insert random data
+INSERT INTO temperature (id, temperature_c, x, y, speed, date) 
+VALUES 
+(1, 23.5, 1.2, 2.3, 3.4, '2019-10-22 12:00:00'),
+(2, 40.5, 2.2, 3.3, 3.4, '2019-10-22 12:01:00'),
+(3, 50.5, 3.2, 4.3, 3.4, '2019-10-22 12:02:00'),
+(4, 40.5, 4.2, 5.3, 2.4, '2019-10-22 12:03:00');
+
+INSERT INTO aceleration (id, x, y, z, timestamp)
+VALUES
+(1, 1.2, 2.3, 3.4, '2019-10-22 12:00:00'),
+(2, 2.2, 3.3, 3.4, '2019-10-22 12:01:00'),
+(3, 3.2, 4.3, 3.4, '2019-10-22 12:02:00'),
+(4, 4.2, 5.3, 2.4, '2019-10-22 12:03:00');
+
+INSERT INTO gps (id, latitude, longitude, timestamp)
+VALUES
+(1, 1.2, 2.3, '2019-10-22 12:00:00'),
+(2, 2.2, 3.3, '2019-10-22 12:01:00'),
+(3, 3.2, 4.3, '2019-10-22 12:02:00'),
+(4, 4.2, 5.3, '2019-10-22 12:03:00');
+
+INSERT INTO race(name, location, track_img_path, date)
+VALUES
+(1, 'Mexico City', 'CDMX', 'img/track1.png', '2019-10-22'),
+(2, 'Mexico City', 'CDMX', 'img/track2.png', '2020-10-22'),
+(3, 'Mexico City', 'CDMX', 'img/track3.png', '2021-10-22'),
+(4, 'Mexico City', 'CDMX', 'img/track4.png', '2022-10-22');
 
 
 -- Querys for average temperature
@@ -50,21 +87,16 @@ WHERE timestamp BETWEEN '2020-10-22 00:00:00' AND '2020-10-22 23:59:59';
 -- Como hacemos para determinar el dia de la carrera?
 
 -- Querys for current temperature
-SELECT temperature_c
+SELECT temperature_c as current_temperature
 FROM temperature
-GROUP BY temperature_c
-Order by DESC LIMIT 1;
+WHERE timestamp = (SELECT MAX(timestamp) FROM temperature);
 
--- SELECT temperature_c, timestamp
--- FROM temperature
--- WHERE timestamp BETWEEN '2020-10-22 00:00:00' AND '2020-10-22 23:59:59'
+-- Querys for current aceleration
+SELECT x, y, z
+FROM aceleration
+WHERE timestamp = (SELECT MAX(timestamp) FROM aceleration);
 
--- Data for temperature
-data(id INT, raceId, date TIMESTAMP, temperature FLOAT, x DOUBLE, y DOUBLE, speed DOUBLE)
-PK(id) AUTO_INCREMENT
-FK(raceId) REFERENCES races(id)
-
--- Data for races
-
-races(id  INT, date DATE, location VARCHAR, track_name VARCHAR, track_img_path VARCHAR)
-PK(id)
+-- Querys for current gps
+SELECT latitude, longitude
+FROM gps
+WHERE timestamp = (SELECT MAX(timestamp) FROM gps);
